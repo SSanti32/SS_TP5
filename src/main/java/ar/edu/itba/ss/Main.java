e5151f283d1dd2e7bb718c1b1fde13b399d77010
Azul <azkim@itba.edu.ar>
2023-06-04 12:00:02 -0300

    FEATURE: Add complete simulation but still not working
    It should be an error where it is divided by zero as results are NaN

--- src/main/java/ar/edu/itba/ss/Main.java
+++ src/main/java/ar/edu/itba/ss/Main.java
@@ -1,7 +1,5 @@
 package ar.edu.itba.ss;
 
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -11,14 +9,15 @@ public class Main {
     public static final String POSITIONS_FILENAME = "positions-1-step.txt";
     public final static String RESOURCES_PATH_SYSTEM = "src/main/java/resources/";
 
+    private static double currentTime = 0;
     public static List<Particle> particles = new ArrayList<>();
-    public static Map<Long, List<Particle>> contacts = new HashMap<>();
+    public static List<Particle> particlesToRemove = new ArrayList<>();
 
     public static void initializeRoom() {
         for(int i = 0; i < Utils.particleCount;) {
             double x = (Math.random() * (20 - Utils.maxRadius)) + Utils.maxRadius;
             double y = (Math.random() * (20 - Utils.maxRadius)) + Utils.maxRadius;
-            Particle p = new Particle(x, y, Utils.minRadius, 0);
+            Particle p = new Particle(x, y, Utils.maxRadius, 0);
             if(!isOverlapping(p)) {
                 particles.add(p);
                 i++;
@@ -28,7 +27,7 @@ public class Main {
 
     public static boolean isOverlapping(Particle p1) {
         for(Particle p2: particles) {
-            if(Math.abs(Utils.getDistance(p1, p2)) < 2 * Utils.minRadius)
+            if(Math.abs(Utils.getParticleDistance(p1, p2)) < 2 * Utils.minRadius)
                 return true;
         }
         return false;
@@ -39,23 +38,119 @@ public class Main {
         initializeRoom();
         FileWriter fileWriter = new FileWriter(FILENAME, true);
         fileWriter.write(Utils.particleCount + "\n");
+        fileWriter.write("Comment" + "\n");
         for(Particle p: particles) {
             fileWriter.write(p.getX() + "\t");
             fileWriter.write(p.getY() + "\t");
             fileWriter.write(p.getRadius() + "\t");
             fileWriter.write("He" + "\n");
         }
+        simulate(fileWriter);
         fileWriter.close();
-
-//        while (particles.size() > 0) {
-//
-//        }
     }
 
-    private static void findContacts(Particle particle) {
-        for (Particle other : particles) {
-            if (Utils.getDistance(particle, other) < Utils.minRadius + Utils.maxRadius)
-                contacts.get(particle.id).add(other);
+    private static void simulate(FileWriter fileWriter) throws IOException {
+        int toPrint = 1;
+        while (particles.size() > 0 && currentTime < 100) {
+
+            // Find contacts and calculate escape velocity
+            for(Particle p1 : particles) {
+                double eij_x = 0;
+                double eij_y = 0;
+                for (Particle p2 : particles) {
+                    if (p1.equals(p2) && Utils.getParticleDistance(p1, p2) >= p1.getRadius() + p2.getRadius()) {
+                        continue;
+                    }
+                    double[] currentEij = p1.getEscapeVelocityDirAndSense(p2);
+                    eij_x += currentEij[0];
+                    eij_y += currentEij[1];
+                }
+
+                // Interaction with walls
+                if (!p1.hasLeftBox) {
+                    // Left wall
+                    if (p1.getX() - p1.getRadius() <= 0) {
+                        eij_x += p1.getX() / Math.sqrt(Math.pow(p1.getX(), 2) + Math.pow(p1.getY(), 2));
+                    }
+
+                    // Right wall
+                    if (p1.getX() + p1.getRadius() >= Utils.wallLength) {
+                        eij_x += (p1.getX() - Utils.wallLength) / Math.sqrt(Math.pow(Utils.wallLength - p1.getX(), 2) + Math.pow(p1.getY(), 2));
+                    }
+
+                    // Bottom wall
+                    if (p1.getY() - p1.getRadius() <= 0) {
+                        if (p1.getX() >= Utils.x_e1 + 0.1 && p1.getX() <= Utils.x_e2 - 0.1) {
+                            p1.setHasLeftBox(true);
+                        } else {
+                            eij_y += p1.getY() / Math.sqrt(Math.pow(p1.getX(), 2) + Math.pow(p1.getY(), 2));
+                        }
+                    }
+
+                    // Top wall
+                    if (p1.getY() + p1.getRadius() >= Utils.wallLength) {
+                        eij_y += (p1.getY() - Utils.wallLength) / Math.sqrt(Math.pow(p1.getX(), 2) + Math.pow(Utils.wallLength - p1.getY(), 2));
+                    }
+                }
+
+                double distanceEij = Math.sqrt(Math.pow(eij_x, 2) + Math.pow(eij_y, 2));
+                p1.setVe(new double[]{eij_x / distanceEij, eij_y / distanceEij});
+                if (eij_x != 0 || eij_y != 0) {
+                    p1.setInContact(true);
+                }
+            }
+
+            // Adjust radii
+            for (Particle p : particles) {
+                if (p.isInContact()) {
+                    p.setRadius(Utils.minRadius);
+                } else {
+                    p.updateRadius();
+                }
+            }
+
+            // Update positions and velocities
+            for (Particle p : particles) {
+                // Compute direction and sense of vd considering current positions and target locations
+                // Compute magnitude of vd depending on the radius
+                if (p.isInContact()) {
+                    p.setV(new double[]{p.getVe()[0] * Utils.maxDesiredSpeed, p.getVe()[1] * Utils.maxDesiredSpeed});
+                } else {
+                    p.calculateDesiredVelocity();
+                    p.setV(new double[]{p.getVd()[0], p.getVd()[1]});
+                }
+
+                // Update position
+                p.setX(p.getX() + p.getV()[0] * Utils.step);
+                p.setY(p.getY() + p.getV()[1] * Utils.step);
+            }
+
+            // Check if particles have left the room
+            for (Particle p : particles) {
+                if (p.hasLeftBox && p.getY() - p.radius <= Utils.targetY
+                        && p.getX() >= Utils.secondTargetX1
+                        && p.getX() <= Utils.secondTargetX2) {
+                    p.setInContact(false);
+                    particlesToRemove.add(p);
+                }
+            }
+
+            // Remove particles that have left the room
+            particles.removeAll(particlesToRemove);
+
+            // Update time
+            currentTime += Utils.step;
+            if (toPrint % 10 == 0) {
+                fileWriter.write(particles.size() + "\n");
+                fileWriter.write("Comment" + "\n");
+                for(Particle p: particles) {
+                    fileWriter.write(p.getX() + "\t");
+                    fileWriter.write(p.getY() + "\t");
+                    fileWriter.write(p.getRadius() + "\t");
+                    fileWriter.write("He" + "\n");
+                }
+            }
+            toPrint++;
         }
     }
 
--- src/main/java/ar/edu/itba/ss/Particle.java
+++ src/main/java/ar/edu/itba/ss/Particle.java
@@ -1,99 +1,132 @@
 package ar.edu.itba.ss;
 
-import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 
+@Getter
+@Setter
 public class Particle {
     private static final AtomicLong ID_GENERATOR = new AtomicLong(0);
     long id;
-    double minRadius, radius, maxRadius;
-    double velocity, escapeVel;
+    double radius;
+    double velocity;
+    double[] v, vd, ve;
+    double[] target;
     double x, y;
-    double speed, escapeSpeed, maxSpeed;
-    double beta;
+    double speed, escapeSpeed, maxSpeed, desiredSpeed;
+    boolean hasLeftBox;
+    boolean inContact;
 
-    double deltaT = minRadius / (2 * Math.max(maxSpeed, escapeSpeed));
-    double tao = 0.5; // check value
 
-    boolean inContact = false; //ESTO DEBERIA SER UNA FUNCION
     public Particle(double x, double y, double radius, double velocity) {
         this.id = ID_GENERATOR.getAndIncrement();
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.velocity = velocity;
-        this.speed = desiredSpeed(Utils.maxSpeed, radius, Utils.minRadius, Utils.maxRadius, Utils.beta);
+        this.escapeSpeed = Utils.maxDesiredSpeed;
+        this.speed = calculateDesiredSpeed(Utils.maxSpeed, radius, Utils.minRadius, Utils.maxRadius, Utils.beta);
+        this.hasLeftBox = false;
+        this.inContact = false;
+        calculateTarget(Utils.x_e1 + 0.2 * Utils.targetLength, Utils.x_e1 + 0.8 * Utils.targetLength);
+        calculateDesiredVelocity();
     }
 
-    public boolean sameStartPosition(Particle p) {
-        double distance = Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2));
-        return distance <= (this.radius + p.radius);
-    }
-    
-    public double getX() {
-        return x;
+    public void calculateDesiredVelocity() {
+        double desiredSpeed = calculateDesiredSpeed(Utils.maxSpeed, this.radius, Utils.minRadius, Utils.maxRadius, Utils.beta);
+        double distance = Math.sqrt(Math.pow(this.target[0], 2) + Math.pow(this.target[1], 2));
+        double[] versor = new double[] {this.target[0] / distance, this.target[1] / distance};
+
+        this.vd = new double[] {desiredSpeed * versor[0], desiredSpeed * versor[1]};
     }
 
-    public double getY() {
-        return y;
+    private double calculateDesiredSpeed(double maxSpeed, double radius, double minRadius, double maxRadius, double beta) {
+        return maxSpeed * Math.pow((radius - minRadius)/(maxRadius - minRadius), beta);
     }
 
-    public double getRadius() {
-        return radius;
-    }
-
-    public static double desiredSpeed(double maxSpeed, double radius, double minRadius, double maxRadius, double beta) {
-        return maxSpeed * Math.pow((radius- minRadius)/(maxRadius - minRadius), beta);
-    }
-
-    public static double[] updatePosition(double prevX, double prevY, double velocity, double deltaT) {
+    public double[] updatePosition(double prevX, double prevY, double velocity, double deltaT) {
         double x = prevX + velocity * deltaT;
         double y = prevY + velocity * deltaT;
         return new double[]{x, y};
     }
 
 
-    public double deltaR() {
-//        this.radius += maxRadius / (tao / deltaT);
-        return (maxRadius / (tao / deltaT));
+    public void updateRadius() {
+        this.radius += Utils.maxRadius / (Utils.tao / Utils.step);
     }
 
-    public double[] calculateEscapeVelocity(List<Particle> otherParticles) {
-        double[] e = new double[] {0, 0};
-        double[] auxE;
-        for (Particle p : otherParticles) {
-            auxE = getEscapeVelocityDirAndSense(p);
-            e[0] += auxE[0];
-            e[1] += auxE[1];
-        }
-        double ve_x = escapeSpeed * (e[0] / Math.abs(e[0]));
-        double ve_y = escapeSpeed * (e[1] / Math.abs(e[1]));
+//    public double[] calculateEscapeVelocity(List<Particle> otherParticles) {
+//        double[] e = new double[] {0, 0};
+//        double[] auxE;
+//        for (Particle p : otherParticles) {
+//            auxE = getEscapeVelocityDirAndSense(p);
+//            e[0] += auxE[0];
+//            e[1] += auxE[1];
+//        }
+//
+//        if (Math.abs(this.x) <= 2 * Utils.maxRadius)
+//            e[0] += (this.x / Math.abs(this.x));
+//        if (Math.abs(this.y) <= 2 * Utils.maxRadius)
+//            e[1] += (this.y / Math.abs(this.y));
+//        if (Math.abs(this.x - Utils.wallLength) <= 2 * Utils.maxRadius)
+//            e[0] += (this.x - Utils.wallLength) / Math.abs(this.x - Utils.wallLength);
+//        if (Math.abs(this.y - Utils.wallLength) <= 2 * Utils.maxRadius)
+//            e[1] += (this.y - Utils.wallLength) / Math.abs(this.y - Utils.wallLength);
+//
+//        double ve_x = escapeSpeed * (e[0] / Math.abs(e[0]));
+//        double ve_y = escapeSpeed * (e[1] / Math.abs(e[1]));
+//
+//        return new double[] {ve_x, ve_y};
+//    }
 
-        return new double[] {ve_x, ve_y};
-    }
-
-    // TODO: Add wall calculations
-    private double[] getEscapeVelocityDirAndSense(Particle other) {
-        double e_x = (this.x - other.x) / Math.abs(this.x - other.x);
-        double e_y = (this.y - other.y) / Math.abs(this.y - other.y);
+    public double[] getEscapeVelocityDirAndSense(Particle other) {
+        double distance = Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+        double e_x = (this.x - other.x) / distance;
+        double e_y = (this.y - other.y) / distance;
 
         return new double[] {e_x, e_y};
     }
 
-    private double[] getEscapeVelocityDirAndSenseForWall(Particle other) {
 
-        double e_x = (this.x - other.x) / Math.abs(this.x - other.x);
-        double e_y = (this.y - other.y) / Math.abs(this.y - other.y);
-
-        return new double[] {e_x, e_y};
-    }
 
     // TODO: Check target x value
-    private double[] getTargetVelocityDirAndSense() {
-        double e_x = (this.x - Utils.wallLength / 2) / Math.abs(this.x - Utils.wallLength / 2);
-        double e_y = (this.x - Utils.targetY) / Math.abs(this.x - Utils.targetY);
+//    private double[] getTargetVelocityDirAndSense() {
+//        double e_x = (this.x - Utils.wallLength / 2) / Math.abs(this.x - Utils.wallLength / 2);
+//        double e_y = (this.x - Utils.targetY) / Math.abs(this.x - Utils.targetY);
+//
+//        return new double[] {e_x, e_y};
+//    }
 
-        return new double[] {e_x, e_y};
+    public void calculateTarget(double start, double end) {
+        double targetX = x;
+        double targetY = Utils.targetY;
+
+        if (x < start || x > end)
+            targetX = getRandomTargetPosition(start, end);
+        if (hasLeftBox)
+            targetY = Utils.secondTargetY;
+
+        target = new double[] {targetX, targetY};
+    }
+
+    private double getRandomTargetPosition(double start, double end) {
+        return start + Math.random() * (end - start);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Particle particle = (Particle) o;
+        return id == particle.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
